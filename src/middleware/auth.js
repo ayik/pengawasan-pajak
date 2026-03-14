dd1c08d783c36b13e6bceadc09bc72e92df4f974
ayik <kojibcell@gmail.com>
2026-03-15 00:11:24 +0700

    Create auth.js with JWT authentication middleware including authenticateToken and authorizeRole functions

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate token
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    if (!token) return res.sendStatus(403);
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+// Middleware to authorize roles
+function authorizeRole(roles) {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.sendStatus(403);
+        }
+        next();
+    };
+}
+
+module.exports = { authenticateToken, authorizeRole };
